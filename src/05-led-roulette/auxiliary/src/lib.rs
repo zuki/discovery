7d5835b71445cef37828c498fdbc6a4f5f60fef0
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-07-30 11:39:07 +0900

    LED rouletteをF4向けに変更

--- src/05-led-roulette/auxiliary/src/lib.rs
+++ src/05-led-roulette/auxiliary/src/lib.rs
@@ -3,28 +3,36 @@
 #![no_std]
 
 #[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust#53964
-extern crate panic_halt; // panic handler
+use panic_halt as _; // panic handler
 
-pub use cortex_m_rt::entry;
-pub use f3::{
-    hal::{delay::Delay, prelude},
-    led::Leds,
+use stm32f407g_disc as board;
+
+use crate::board::{
+    hal::stm32,
+    hal::prelude::*,
 };
 
-use f3::hal::{prelude::*, stm32f30x};
+pub use crate::board::{
+    hal::{delay::Delay, prelude::_embedded_hal_blocking_delay_DelayMs},
+    led::{LedColor, Leds},
+};
+
+use cortex_m::peripheral::Peripherals;
+pub use cortex_m_rt::entry;
 
 pub fn init() -> (Delay, Leds) {
-    let cp = cortex_m::Peripherals::take().unwrap();
-    let dp = stm32f30x::Peripherals::take().unwrap();
+    let cp = Peripherals::take().unwrap();
+    let p = stm32::Peripherals::take().unwrap();
 
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
-
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
+    let gpiod = p.GPIOD.split();
+    // ボード上のUser LEDを初期化
+    let leds = Leds::new(gpiod);
+    // クロックレジスタをconstrain
+    let rcc = p.RCC.constrain();
+    // クロックを最大周波数168MHzに設定してフリーズする
+    let clocks = rcc.cfgr.sysclk(168.mhz()).freeze();
+    // delayプロバイダを取得
     let delay = Delay::new(cp.SYST, clocks);
-
-    let leds = Leds::new(dp.GPIOE.split(&mut rcc.ahb));
 
     (delay, leds)
 }
--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,40 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+// _embedded_hal_blocking_delay_DelayMsはdelay_ms()に必要
+use aux5::{entry, _embedded_hal_blocking_delay_DelayMs, Delay, Leds};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, Leds) = aux5::init();
+
+    let ms = 50_u16;
 
     // infinite loop; just so we don't leave this stack frame
-    loop {}
+    loop {
+        for curr in 0..4 {
+            let next = (curr + 1) % 4;
+
+            leds[next].on();
+            delay.delay_ms(ms);
+            leds[curr].off();
+            delay.delay_ms(ms);
+        }
+    /*
+        leds[LedColor::Orange].on();
+        delay.delay_ms(ms);
+        leds[LedColor::Red].on();
+        delay.delay_ms(ms);
+        leds[LedColor::Blue].on();
+        delay.delay_ms(ms);
+        leds[LedColor::Orange].off();
+        leds[LedColor::Green].on();
+        delay.delay_ms(ms);
+        leds[LedColor::Red].off();
+        delay.delay_ms(ms);
+        leds[LedColor::Blue].off();
+        delay.delay_ms(ms);
+        leds[LedColor::Green].off();
+    */
+    }
 }
