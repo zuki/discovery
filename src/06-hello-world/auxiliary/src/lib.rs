b8dfe93ef50ba75e753ef87c21fea0449cea3815
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-07-30 16:42:53 +0900

    6. Hello, worldを変更

--- src/06-hello-world/auxiliary/src/lib.rs
+++ src/06-hello-world/auxiliary/src/lib.rs
@@ -3,15 +3,23 @@
 #![no_std]
 
 #[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust#53964
-extern crate f3; // provides memory.x
+
 #[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust#53964
 extern crate panic_itm; // panic handler
+
+use stm32f407g_disc as board;
+use board::hal::prelude::*;
+use board::hal::stm32;
 
 pub use cortex_m::{asm::bkpt, iprint, iprintln, peripheral::ITM};
 pub use cortex_m_rt::entry;
 
 pub fn init() -> ITM {
+    let bp = stm32::Peripherals::take().unwrap();
     let p = cortex_m::Peripherals::take().unwrap();
+
+    let rcc = bp.RCC.constrain();
+    rcc.cfgr.sysclk(168.mhz()).freeze();
 
     p.ITM
 }
