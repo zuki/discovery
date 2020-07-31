77c029374923ee974584ec722b1b96dd04bebf2e
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-07-31 11:37:46 +0900

    最適化にwrite_volatileで対応

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -1,6 +1,8 @@
 #![no_main]
 #![no_std]
 
+use core::ptr;
+
 #[allow(unused_imports)]
 use aux7::{entry, iprint, iprintln};
 
@@ -13,16 +15,16 @@ fn main() -> ! {
         const GPIOD_BSRR: u32 = 0x4002_0C18;
 
         // Turn on the "North" LED (orange)
-        *(GPIOD_BSRR as *mut u32) = 1 << 13;
+        ptr::write_volatile(GPIOD_BSRR as *mut u32, 1 << 13);
 
         // Turn on the "East" LED (red)
-        *(GPIOD_BSRR as *mut u32) = 1 << 14;
+        ptr::write_volatile(GPIOD_BSRR as *mut u32, 1 << 14);
 
         // Turn off the "North" LED
-        *(GPIOD_BSRR as *mut u32) = 1 << (13 + 16);
+        ptr::write_volatile(GPIOD_BSRR as *mut u32, 1 << (13 + 16));
 
         // Turn off the "East" LED
-        *(GPIOD_BSRR as *mut u32) = 1 << (14 + 16);
+        ptr::write_volatile(GPIOD_BSRR as *mut u32, 1 << (14 + 16));
     }
 
     loop {}
