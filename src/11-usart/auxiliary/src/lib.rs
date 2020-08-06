5689f85660711051fc3a1259de0a6e8d6dee316e
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-08-06 15:43:27 +0900

    計時機能を実装

--- src/11-usart/auxiliary/src/lib.rs
+++ src/11-usart/auxiliary/src/lib.rs
@@ -9,26 +9,30 @@
 #[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust53964
 extern crate panic_itm; // panic handler
 
-pub use cortex_m::{asm::bkpt, iprint, iprintln, peripheral::ITM};
+pub use cortex_m::{asm::bkpt, iprint, iprintln, peripheral::{ITM, DWT}};
 pub use cortex_m_rt::entry;
 pub use stm32f4xx_hal::{
     prelude,
     serial::{config::Config, Serial},
     stm32::usart1,
+    dwt::Dwt,
 };
 
 use stm32f4xx_hal::{
     prelude::*,
     stm32::{self, USART2},
+    dwt::{DwtExt},
 };
 
-pub fn init() -> (&'static mut usart1::RegisterBlock, ITM) {
+pub fn init() -> (&'static mut usart1::RegisterBlock, Dwt, ITM) {
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = stm32::Peripherals::take().unwrap();
 
     let rcc = dp.RCC.constrain();
 
     let clocks = rcc.cfgr.sysclk(168.mhz()).freeze();
+
+    let dwt = cp.DWT.constrain(cp.DCB, clocks);
 
     let gpioa = dp.GPIOA.split();
 
@@ -45,6 +49,7 @@ pub fn init() -> (&'static mut usart1::RegisterBlock, ITM) {
     unsafe {
         (
             &mut *(USART2::ptr() as *mut _),
+            dwt,
             cp.ITM,
         )
     }
--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -7,13 +7,26 @@ use aux11::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    let (usart, _itm) = aux11::init();
+    let (usart, dwt, mut itm) = aux11::init();
+
+    let mut lap_times = [0u32; 2];
+    let mut sw = dwt.stopwatch(&mut lap_times);
 
     // Send a single character
     for byte in b"The quick brown fox jumps over the lazy dog.".iter() {
         while usart.sr.read().txe().bit_is_clear() {}
 
         usart.dr.write(|w| w.dr().bits(u16::from(*byte)));
+    }
+
+    sw.lap();
+    if let Some(lap_time) = sw.lap_time(1) {
+        iprintln!(
+            &mut itm.stim[0],
+            "for loop took {} ticks ({} us)",
+            lap_time.as_ticks(),
+            lap_time.as_micros()
+        );
     }
 
     loop {}
