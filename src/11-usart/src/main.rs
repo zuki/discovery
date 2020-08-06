d543642f0fb088eb4abf1797f4029dba0a78dbf1
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-08-06 18:31:25 +0900

    11.7 文字列反転を実装

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -5,58 +5,37 @@
 // use core::fmt::{self, Write};
 
 #[allow(unused_imports)]
-use aux11::{entry, iprint, iprintln, usart1};
+use aux11::{entry, iprint, iprintln};
 
-/*
-macro_rules! uprint {
-    ($serial:expr, $($arg:tt)*) => {
-        $serial.write_fmt(format_args!($($arg)*)).ok()
-    };
-}
-
-macro_rules! uprintln {
-    ($serial:expr, $fmt:expr) => {
-        uprint!($serial, concat!($fmt, "\r\n"))
-    };
-    ($serial:expr, $fmt:expr, $($arg:tt)*) => {
-        uprint!($serial, concat!($fmt, "\r\n"), $($arg)*)
-    };
-}
-
-struct SerialPort {
-    usart1: &'static mut usart1::RegisterBlock,
-}
-
-impl fmt::Write for SerialPort {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        for byte in s.bytes() {
-            while self.usart1.sr.read().txe().bit_is_clear() {}
-
-            self.usart1.dr.write(|w| w.dr().bits(u16::from(byte)));
-        }
-        Ok(())
-    }
-}
-*/
+use heapless::{consts, Vec};
 
 #[entry]
 fn main() -> ! {
     let (usart, _dwt, _itm) = aux11::init();
 
-    //let mut serial = SerialPort { usart1: usart };
-
-    //uprintln!(serial, "The anser is {}", 40 + 2);
+    let mut buffer: Vec<u8, consts::U32> = Vec::new();
 
     loop {
-        while usart.sr.read().rxne().bit_is_clear() {}
-        let mut byte = usart.dr.read().dr().bits() as u8;
-    /*
-        byte += 1;
-        if byte > b'~' {
-            byte = b' ';
+        buffer.clear();
+
+        loop {
+            while usart.sr.read().rxne().bit_is_clear() {}
+            let byte = usart.dr.read().dr().bits() as u8;
+            if buffer.push(byte).is_err() {
+                for byte in b"error: buffer full\n\r" {
+                    while usart.sr.read().txe().bit_is_clear() {}
+                    usart.dr.write(|w| w.dr().bits(u16::from(*byte)));
+                }
+                break;
+            }
+            // byte == \r
+            if byte == 13 {
+                for byte in buffer.iter().rev().chain(&[b'\r', b'\n']) {
+                    while usart.sr.read().txe().bit_is_clear() {}
+                    usart.dr.write(|w| w.dr().bits(u16::from(*byte)));
+                }
+                break;
+            }
         }
-    */
-        while usart.sr.read().txe().bit_is_clear() {}
-        usart.dr.write(|w| w.dr().bits(u16::from(byte)));
     }
 }
