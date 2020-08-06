e6157349aa0b064e9f448bd0aa53c771191d01db
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-08-06 17:23:53 +0900

    11.4 uprintln!を実装

--- src/11-usart/openocd.gdb
+++ src/11-usart/openocd.gdb
@@ -17,7 +17,7 @@ break rust_begin_unwind
 # end
 
 # *try* to stop at the user entry point (it might be gone due to inlining)
-break main
+#break main
 
 monitor arm semihosting enable
 
@@ -38,4 +38,4 @@ monitor itm port 0 on
 load
 
 # start the process but immediately halt the processor
-stepi
+continue
--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -2,32 +2,48 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::{self, Write};
+
 #[allow(unused_imports)]
-use aux11::{entry, iprint, iprintln};
+use aux11::{entry, iprint, iprintln, usart1};
+
+macro_rules! uprint {
+    ($serial:expr, $($arg:tt)*) => {
+        $serial.write_fmt(format_args!($($arg)*)).ok()
+    };
+}
+
+macro_rules! uprintln {
+    ($serial:expr, $fmt:expr) => {
+        uprint!($serial, concat!($fmt, "\r\n"))
+    };
+    ($serial:expr, $fmt:expr, $($arg:tt)*) => {
+        uprint!($serial, concat!($fmt, "\r\n"), $($arg)*)
+    };
+}
+
+struct SerialPort {
+    usart1: &'static mut usart1::RegisterBlock,
+}
+
+impl fmt::Write for SerialPort {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for byte in s.bytes() {
+            while self.usart1.sr.read().txe().bit_is_clear() {}
+
+            self.usart1.dr.write(|w| w.dr().bits(u16::from(byte)));
+        }
+        Ok(())
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    let (usart, dwt, mut itm) = aux11::init();
+    let (usart, _dwt, _itm) = aux11::init();
 
-    let mut lap_times = [0u32; 2];
-    let mut sw = dwt.stopwatch(&mut lap_times);
+    let mut serial = SerialPort { usart1: usart };
 
-    // Send a single character
-    for byte in b"The quick brown fox jumps over the lazy dog.".iter() {
-        while usart.sr.read().txe().bit_is_clear() {}
-
-        usart.dr.write(|w| w.dr().bits(u16::from(*byte)));
-    }
-
-    sw.lap();
-    if let Some(lap_time) = sw.lap_time(1) {
-        iprintln!(
-            &mut itm.stim[0],
-            "for loop took {} ticks ({} us)",
-            lap_time.as_ticks(),
-            lap_time.as_micros()
-        );
-    }
+    uprintln!(serial, "The anser is {}", 40 + 2);
 
     loop {}
 }
